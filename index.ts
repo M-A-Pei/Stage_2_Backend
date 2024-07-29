ddbce14a62445ef2bf2798c801f937f19aedde6c
Muhammad Abdullah Syafii <syafii2006@gmail.com>
2024-07-30 06:57:47 +0700

    init server

--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
+import { send } from "process"
 
 const app = express()
 dotenv.config()
 
+app.get("/", (req, res) => {
+    res.send("hiiii")
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log(`server is running at http://localhost:${port}`)
+})

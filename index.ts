0f2734b6e7809eb533059b1a6724d01457d5fa64
Muhammad Abdullah Syafii <syafii2006@gmail.com>
2024-09-02 12:28:22 +0700

    vercel

--- index.ts
+++ index.ts
@@ -10,18 +10,22 @@ const app = express()
 dotenv.config()
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use("/uploads", express.static(path.join(__dirname, "/src", "/uploads")))
 
 app.use(route)
 
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
 const port = process.env.PORT || 3000
-app.listen(port, async ()=>{
-    try{
+app.listen(port, async () => {
+    try {
         await db.$connect()
         console.log(`server is running at http://localhost:${port}`)
-    }catch{
-         await db.$disconnect()
+    } catch {
+        await db.$disconnect()
     }
 })
